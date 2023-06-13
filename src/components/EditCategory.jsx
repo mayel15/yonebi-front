1bb25d13eba9cbd6f045bb32ea2c986fee9c9e3d
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-06-13 02:44:48 +0200

    add edit cat & subject

--- src/components/EditCategory.jsx
+++ src/components/EditCategory.jsx
@@ -1,8 +1,9 @@
 import { React, useState, useEffect } from "react";
 
-export default function EditSubject(props) {
+export default function EditCategory(props) {
     const [formData, setFormData] = useState({
         name: props.name,
+        subject: props.subject
     });
 
     const handleChange = (event) => {
@@ -18,7 +19,7 @@ export default function EditSubject(props) {
         if(formData.name === "" ){
             alert('Au moins un champ du formulaire est vide. :(')
         }else{
-        await fetch(`https://yonebi-back.vercel.app/api/subjects/${props.id}`, {
+        await fetch(`https://yonebi-back.vercel.app/api/categories/${props.id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
@@ -30,7 +31,7 @@ export default function EditSubject(props) {
                 console.log(data);
                 if(data.message){
                     alert(data.message)
-                    window.location.href = '/admin/subjects'
+                    window.location.href = '/admin/categories'
                 }
                 
             })
@@ -40,14 +41,17 @@ export default function EditSubject(props) {
         }
     };
 
-
     return (
         <div className="text-center">
 
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="name">Sujet</label>
-                    <input type='text' name="name" value={formData.name} placeholder="Sujet" onChange={handleChange} />
+                    <label htmlFor="name">Catégories</label>
+                    <input type='text' name="name" value={formData.name} placeholder="Catégorie" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="subject">Sujet</label>
+                    <input type='text' name="subject" value={formData.subject} placeholder="Sujet" onChange={handleChange} />
                 </div>
 
                 <button
